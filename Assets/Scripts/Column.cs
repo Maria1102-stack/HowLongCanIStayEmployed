91132496cfb960b5c5b61d16262da6bc09722da8
Maria Holm <mariaholmp@yahoo.dk>
2026-06-13 03:33:47 +0200

    slots added to columns

--- Assets/Scripts/Column.cs
+++ Assets/Scripts/Column.cs
@@ -3,9 +3,36 @@ using UnityEngine;
 public class Column : MonoBehaviour
 {
     [SerializeField] private ProductData acceptedProduct;
+    [SerializeField] private Transform[] slots;
 
     public ProductData AcceptedProduct
     {
         get { return acceptedProduct; }
+    }
+
+    public Transform[] Slots
+    {
+        get { return slots; }
+    }
+
+    public void Fill()
+    {
+        foreach (Transform slot in slots)
+        {
+            if(slot.childCount == 0)
+            {
+                Instantiate(
+                    acceptedProduct.Prefab,
+                    slot.position,
+                    slot.rotation,
+                    slot
+                );
+            }
+        }
+    }
+
+    private void Start()
+    {
+        Fill();
     }
 }
